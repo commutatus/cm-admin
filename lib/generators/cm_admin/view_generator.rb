d476254842fe922eed7e8f92b7044b401a05bcb8
Anbazhagan <anbublacky@gmail.com>
2020-05-29 13:26:34 +0530

    fix: added a module, So that the copy methods are not called twice
    - Added description to view generator

--- lib/generators/cm_admin/view_generator.rb
+++ lib/generators/cm_admin/view_generator.rb
@@ -1,23 +1,6 @@
 module CmAdmin
-  class ViewGenerator < Rails::Generators::NamedBase
-    source_root File.expand_path('templates', __dir__)
-
-    argument :page_type, required: true, default: nil,
-                         desc: "The scope to copy views to"
-    class_option :column_names, aliases: "-c", type: :array, desc: "Select specific view directories to generate (confirmations, passwords, registrations, sessions, unlocks, mailer)"
-
-    def create_new_file
-      if page_type == 'index'
-        copy_index_files
-      elsif page_type == 'form'
-        copy_show_files
-      else
-        puts "Wrong page_type provided. It can be either index or show"
-      end
-    end
-
+  module ViewActionTemplate
     def copy_index_files
-      puts "came here #{options[:column_names]}"
       template "views/index.erb", "app/views/admin/#{name}/index.html.slim"
       template "views/_table.erb", "app/views/admin/#{name}/_table.html.slim"
       copy_file "views/_filters.html.slim", "app/views/admin/#{name}/_filters.html.slim"
@@ -30,5 +13,41 @@ module CmAdmin
     def copy_form_files
       template "views/_form.erb", "app/views/admin/#{name}/_form.html.slim"
     end
+  end
+  class ViewGenerator < Rails::Generators::NamedBase
+    include ViewActionTemplate
+    desc <<-DESC.strip_heredoc
+      Generate views for specific action of the model in admin
+      Use -c to specify which column names you want to be present on page.
+      Atleast one option is mandatory.
+      For example:
+        rails g cm_admin:view NAME page_type -c options
+        rails g cm_admin:view products form -c title:string price:integer
+        rails g cm_admin:view products index -c title price
+
+      NAME is the table name
+      page_type is the page you want to copy files. It can be the following
+      index, show, form.
+
+      This will create a files respectively at app/views/admin/#{name}/xxx.html.slim
+
+    DESC
+
+    source_root File.expand_path('templates', __dir__)
+
+    argument :page_type, required: true, default: nil,
+                         desc: "The scope to copy views to"
+    class_option :column_names, aliases: "-c", type: :array, desc: "Select specific view directories to generate (confirmations, passwords, registrations, sessions, unlocks, mailer)"
+
+    def create_new_file
+      if page_type == 'index'
+        copy_index_files
+      elsif page_type == 'form'
+        copy_form_files
+      else
+        puts "Wrong page_type provided. It can be either index or show"
+      end
+    end
+
   end
 end
