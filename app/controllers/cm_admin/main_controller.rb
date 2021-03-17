f1f2b4aedb387d7aa469a9e18c82218e6eaf33bd
Anbazhagan <anbublacky@gmail.com>
2021-03-17 18:13:11 +0530

    Assets added and loaded on to the project.

--- app/controllers/cm_admin/main_controller.rb
+++ app/controllers/cm_admin/main_controller.rb
@@ -1,5 +1,6 @@
 module CmAdmin
   class MainController < CmAdmin::ApplicationController
+    layout 'cm_admin'
     def dashboard
 
     end
--- lib/cm_admin/engine.rb
+++ lib/cm_admin/engine.rb
@@ -2,5 +2,12 @@ require 'rails'
 module CmAdmin
   class Engine < Rails::Engine
     isolate_namespace CmAdmin
+
+    initializer 'RailsAdmin precompile hook', group: :all do |app|
+      app.config.assets.precompile += %w(
+        cm_admin/cm_admin.css
+      )
+    end
+
   end
 end
