4ec324f1139c0118dd6da643d10e6f13fc5159c3
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2021-06-02 19:46:03 +0530

    Chore: Raise ArgumentError for invalid filter type

--- lib/cm_admin/models/filter.rb
+++ lib/cm_admin/models/filter.rb
@@ -6,6 +6,7 @@ module CmAdmin
       VALID_FILTER_TYPES = [:search, :date, :range, :dropdown, :checkbox].freeze
 
       def initialize(db_column_name:, filter_type:, options: {})
+        raise ArgumentError, "Kindly select a valid filter type like #{VALID_FILTER_TYPES.to_sentence} instead of #{filter_type} for column #{db_column_name}" unless VALID_FILTER_TYPES.include?(filter_type.to_sym)
         @name = db_column_name
         @type = filter_type.to_sym
         options.each do |key, value|
