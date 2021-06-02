291600fba23aad18ab4c7dc7f40b5a81f4929588
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2021-06-02 19:37:44 +0530

    Chore: attr_accessor for filters

--- lib/cm_admin/models/filter.rb
+++ lib/cm_admin/models/filter.rb
@@ -1,6 +1,8 @@
 module CmAdmin
   module Models
     class Filter
+      attr_accessor :db_column_name, :filter_type, :placeholder, :collection, :multiselect, :checked
+
       def initialize(db_column_name:, filter_type:, options: {})
         @name = db_column_name
         @type = filter_type.to_sym
