8e19b20208eaef608687047c66987468122f9970
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2021-06-02 19:36:08 +0530

    Chore: Filter initializer

--- lib/cm_admin/models/filter.rb
+++ lib/cm_admin/models/filter.rb
@@ -1,6 +1,13 @@
 module CmAdmin
   module Models
     class Filter
+      def initialize(db_column_name:, filter_type:, options: {})
+        @name = db_column_name
+        @type = filter_type.to_sym
+        options.each do |key, value|
+          self.send("#{key.to_s}=", value)
+        end
+      end
     end
   end
 end
