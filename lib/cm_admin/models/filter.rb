25a542a5a91f2d05a0f536eaccd66bd61c980058
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2021-06-03 13:09:36 +0530

    Refactor: Convert the VALID_FILTER_TYPES constant to set from array

--- lib/cm_admin/models/filter.rb
+++ lib/cm_admin/models/filter.rb
@@ -3,7 +3,7 @@ module CmAdmin
     class Filter
       attr_accessor :db_column_name, :filter_type, :placeholder, :collection, :multiselect, :checked
 
-      VALID_FILTER_TYPES = [:search, :date, :range, :dropdown, :checkbox].freeze
+      VALID_FILTER_TYPES = Set[:checkbox, :date, :dropdown, :range, :search].freeze
 
       def initialize(db_column_name:, filter_type:, options: {})
         raise ArgumentError, "Kindly select a valid filter type like #{VALID_FILTER_TYPES.to_sentence} instead of #{filter_type} for column #{db_column_name}" unless VALID_FILTER_TYPES.include?(filter_type.to_sym)
