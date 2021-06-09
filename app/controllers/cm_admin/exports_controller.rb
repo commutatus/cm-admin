1370cbff9453f140984b80cd2425aa92abe93d3f
charith08 <charithsaivadla@gmail.com>
2021-06-10 00:15:33 +0530

    chore: dropdown UI

--- app/controllers/cm_admin/exports_controller.rb
+++ app/controllers/cm_admin/exports_controller.rb
@@ -9,15 +9,7 @@ module CmAdmin
 
     def export
       file_path = CmAdmin::Models::Export.generate_excel(params[:class_name], params[:columns])
-      send_file file_path, :disposition => 'attachment'
-    end
-
-    def ajax_download
-      file = File.open(params[:file_path], "rb")
-      contents = file.read
-      file.close
-      send_data(contents, :filename => params[:file_path].split('/').last)
-      File.delete(params[:file_path]) if File.exist?(params[:file_path])
+      send_file file_path, disposition: 'attachment'
     end
 
   end
