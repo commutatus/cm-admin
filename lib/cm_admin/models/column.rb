1c19ff7be1af2c570882f7269fb367f313efbfe2
charith08 <charithsaivadla@gmail.com>
2021-06-18 01:47:16 +0530

    chore: formatting header during initialisation

--- lib/cm_admin/models/column.rb
+++ lib/cm_admin/models/column.rb
@@ -8,6 +8,18 @@ module CmAdmin
         attributes.each do |key, value|
           self.send("#{key.to_s}=", value)
         end
+
+        #formatting header (either db_column_name or value present in header attribute)
+        header_value = format_header
+        self.send("header=", header_value)
+
+        #all columns are exportable by default
+        @exportable = true if attributes[:exportable].nil?
+      end
+
+      #return a string value as a header (either db_column_name or value present in header attribute)
+      def format_header
+        self.header.present? ? self.header.to_s.gsub(/_/, ' ')&.upcase : self.db_column_name.to_s.gsub(/_/, ' ').upcase
       end
 
     end
