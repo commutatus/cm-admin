e84277b18e3d46d64d237fa3dbdfcd68a5eb6c21
charith08 <charithsaivadla@gmail.com>
2021-06-18 12:04:53 +0530

    chore: adding suffix, round to attr_accessor

--- lib/cm_admin/models/column.rb
+++ lib/cm_admin/models/column.rb
@@ -1,7 +1,7 @@
 module CmAdmin
   module Models
     class Column
-      attr_accessor :db_column_name, :column_type, :header, :format, :prefix, :exportable
+      attr_accessor :db_column_name, :column_type, :header, :format, :prefix, :suffix, :exportable, :round
 
       def initialize(db_column_name, attributes = {})
         @db_column_name = db_column_name
@@ -10,8 +10,7 @@ module CmAdmin
         end
 
         #formatting header (either db_column_name or value present in header attribute)
-        header_value = format_header
-        self.send("header=", header_value)
+        self.send("header=", format_header)
 
         #all columns are exportable by default
         @exportable = true if attributes[:exportable].nil?
