a016b162f6309207372893b7444408ef9ae1faca
Anbazhagan <anbublacky@gmail.com>
2021-06-22 17:33:45 +0530

    fix: removed unwanted file generator, added datatype support to form field
    - added the initializer back

--- lib/cm_admin/view_helpers/form_field_helper.rb
+++ lib/cm_admin/view_helpers/form_field_helper.rb
@@ -4,17 +4,19 @@ module CmAdmin
       def input_field_for_column(f, field)
         case field.input_type
         when :integer
-          return f.number_field field.field_name, class: 'normal-input integer-only'
+          return f.number_field field.field_name, class: 'normal-input', data: {behaviour: 'integer-only'}
+        when :decimal
+          return f.number_field field.field_name, class: 'normal-input', data: {behaviour: 'decimal-only'}
         when :string
           return f.text_field field.field_name, class: 'normal-input'
         when :single_select
           return f.select field.field_name, options_for_select(field.collection || []), {}, class: 'normal-input select-2'
         when :multi_select
-          return f.select field.field_name, options_for_select(field.collection || []), {}, class: 'select-2', multiple: true
+          return f.select field.field_name, options_for_select(field.collection || []), {}, class: 'normal-input select-2', multiple: true
         when :date
-          return f.text_field field.field_name, class: 'normal-input date-only', data: {behaviour: 'date-only'}
+          return f.text_field field.field_name, class: 'normal-input', data: {behaviour: 'date-only'}
         when :date_time
-          return f.text_field field.field_name, class: 'normal-input date-time'
+          return f.text_field field.field_name, class: 'normal-input', data: {behaviour: 'date-time'}
         when :text
           return f.text_area field.field_name, class: 'normal-input'
         end
