ed6d223bf32b3b4d228985c2293f23e0aadf7cb8
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2021-07-07 18:57:22 +0530

    Feat: Method to generate range filter

--- lib/cm_admin/view_helpers/filter_helper.rb
+++ lib/cm_admin/view_helpers/filter_helper.rb
@@ -2,6 +2,16 @@ module CmAdmin
   module ViewHelpers
     module FilterHelper
 
+      def add_range_filter(filter)
+        concat(content_tag(:div, class: 'filter-chips-wrapper hidden') do
+          concat(content_tag(:div, class: 'filter-chip d-flex') do
+            concat tag.input type: 'number', min: '0', step: '1', class: 'normal-input', placeholder: 'From', data: {behaviour: 'filter', filter_type: "#{filter.filter_type}", db_column: "#{filter.db_column_name}"}
+            concat tag.input type: 'number', min: '0', step: '1', class: 'normal-input ml-2', placeholder: 'To', data: {behaviour: 'filter', filter_type: "#{filter.filter_type}", db_column: "#{filter.db_column_name}"}
+          end)
+        end)
+        return
+      end
+
       def add_date_filter(filter)
         concat(content_tag(:div, class: 'filter-chips-wrapper hidden') do
           concat(content_tag(:div, class: 'filter-chip') do
