e4c73418cb51a6463a8f77dde29aab6a02b6b52b
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2021-07-07 18:56:43 +0530

    Feat: Method to generate date filter

--- lib/cm_admin/view_helpers/filter_helper.rb
+++ lib/cm_admin/view_helpers/filter_helper.rb
@@ -2,6 +2,14 @@ module CmAdmin
   module ViewHelpers
     module FilterHelper
 
+      def add_date_filter(filter)
+        concat(content_tag(:div, class: 'filter-chips-wrapper hidden') do
+          concat(content_tag(:div, class: 'filter-chip') do
+            concat tag.input class: 'normal-input', placeholder: "#{filter.placeholder}", data: {behaviour: 'filter', filter_type: "#{filter.filter_type}", db_column: "#{filter.db_column_name}"}
+          end)
+        end)
+        return
+      end
     end
   end
 end
