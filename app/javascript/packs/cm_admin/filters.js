ab372665f3333a5a2405d924ddce387eb8654865
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2021-07-08 18:00:53 +0530

    Chore: Method to get params as object

--- app/javascript/packs/cm_admin/filters.js
+++ app/javascript/packs/cm_admin/filters.js
@@ -67,3 +67,51 @@ $(document).on('keyup', '#cm-add-filter-search', function(e){
     }
   }
 });
+
+// Method to decode the encoded nested and/or complex hash and convert it to
+// object that is used for filters while sending the data to the backend.
+var getParamsAsObject = function (query) {
+  query = query.substring(query.indexOf('?') + 1);
+
+  var re = /([^&=]+)=?([^&]*)/g;
+  var decodeRE = /\+/g;
+
+  var decode = function (str) {
+    return decodeURIComponent(str.replace(decodeRE, " "));
+  };
+
+  var params = {}, e;
+  while (e = re.exec(query)) {
+    var k = decode(e[1]), v = decode(e[2]);
+    if (k.substring(k.length - 2) === '[]') {
+      k = k.substring(0, k.length - 2);
+      (params[k] || (params[k] = [])).push(v);
+    }
+    else params[k] = v;
+  }
+
+  var assign = function (obj, keyPath, value) {
+    var lastKeyIndex = keyPath.length - 1;
+    for (var i = 0; i < lastKeyIndex; ++i) {
+      var key = keyPath[i];
+      if (!(key in obj))
+        obj[key] = {}
+      obj = obj[key];
+    }
+    obj[keyPath[lastKeyIndex]] = value;
+  }
+
+  for (var prop in params) {
+    var structure = prop.split('[');
+    if (structure.length > 1) {
+      var levels = [];
+      structure.forEach(function (item, i) {
+        var key = item.replace(/[?[\]\\ ]/g, '');
+        levels.push(key);
+      });
+      assign(params, levels, params[prop]);
+      delete(params[prop]);
+    }
+  }
+  return params;
+};
