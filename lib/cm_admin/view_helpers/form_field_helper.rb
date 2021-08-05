5a3c803b372ff3a2d0e0b24e8e25a68336870814
Anbazhagan <anbublacky@gmail.com>
2021-08-05 13:23:12 +0530

    fix: form disabled support added

--- lib/cm_admin/view_helpers/form_field_helper.rb
+++ lib/cm_admin/view_helpers/form_field_helper.rb
@@ -4,19 +4,19 @@ module CmAdmin
       def input_field_for_column(f, field)
         case field.input_type
         when :integer
-          return f.text_field field.field_name, class: 'normal-input', data: {behaviour: 'integer-only'}
+          return f.text_field field.field_name, class: 'normal-input', disabled: field.disabled, data: {behaviour: 'integer-only'}
         when :decimal
-          return f.text_field field.field_name, class: 'normal-input', data: {behaviour: 'decimal-only'}
+          return f.text_field field.field_name, class: 'normal-input', disabled: field.disabled, data: {behaviour: 'decimal-only'}
         when :string
-          return f.text_field field.field_name, class: 'normal-input'
+          return f.text_field field.field_name, class: 'normal-input', disabled: field.disabled
         when :single_select
           return f.select field.field_name, options_for_select(field.collection || []), {}, class: 'normal-input select-2'
         when :multi_select
           return f.select field.field_name, options_for_select(field.collection || []), {}, class: 'normal-input select-2', multiple: true
         when :date
-          return f.text_field field.field_name, class: 'normal-input', data: {behaviour: 'date-only'}
+          return f.text_field field.field_name, class: 'normal-input', disabled: field.disabled, data: {behaviour: 'date-only'}
         when :date_time
-          return f.text_field field.field_name, class: 'normal-input', data: {behaviour: 'date-time'}
+          return f.text_field field.field_name, class: 'normal-input', disabled: field.disabled, data: {behaviour: 'date-time'}
         when :text
           return f.text_area field.field_name, class: 'normal-input'
         when :single_file_upload
