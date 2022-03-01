8d81145fafe0229c60c7c1a8e2ee26592b781ce5
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-03-01 21:22:06 +0530

    Feat: DSL methods for sort column + validation

--- lib/cm_admin/models/dsl_method.rb
+++ lib/cm_admin/models/dsl_method.rb
@@ -137,6 +137,13 @@ module CmAdmin
 
         @current_action.sort_direction = direction.to_sym if @current_action
       end
+
+      def sort_column(column = :created_at)
+        db_columns = self.instance_variable_get(:@ar_model)&.columns&.map{|x| x.name.to_sym}
+        raise "Sorting for custom column #{column} does not exist." unless db_columns.include?(column.to_sym)
+
+        @current_action.sort_column = column.to_sym if @current_action
+      end
     end
   end
 end
