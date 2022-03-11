5d3af9588fa56bf0f0b12b5939c1f976313d1d58
anbublacky <anbublacky@gmail.com>
2022-03-11 19:53:56 +0530

    Setting dynamic icon for model (#135)

--- lib/cm_admin/view_helpers/navigation_helper.rb
+++ lib/cm_admin/view_helpers/navigation_helper.rb
@@ -11,7 +11,7 @@ module CmAdmin
               content_tag(:a, href: path) do
                 content_tag(:div, class: 'menu-item') do
                   content_tag(:span, class: 'menu-icon') do
-                    concat tag.i class: 'fa fa-th-large'
+                    concat tag.i class: "#{model.icon_name}"
                   end +
                   model.name.pluralize
                 end
@@ -20,7 +20,7 @@ module CmAdmin
               content_tag(:a, href: path, class: 'visible') do
                 content_tag(:div, class: 'result-item') do
                   content_tag(:span) do
-                    concat tag.i class: 'fa fa-th-large'
+                    concat tag.i class: "#{model.icon_name}"
                   end +
                   content_tag(:span) do
                     model.name
