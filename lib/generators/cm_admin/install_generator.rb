a453d5555ebccaa7a8b5ba0c50dbbf8e66707381
anbublacky <anbublacky@gmail.com>
2022-03-17 21:51:37 +0530

    fix: Adding the initializer back

--- lib/generators/cm_admin/install_generator.rb
+++ lib/generators/cm_admin/install_generator.rb
@@ -1,27 +1,14 @@
 require 'rails/generators'
+
 module CmAdmin
-  class InstallGenerator < Rails::Generators::Base
-    def add_required_gems
-      gem 'slim'
-      gem 'simple_form'
-      system("yarn add bootstrap")
-      system("yarn add @fortawesome/fontawesome-free")
-      system("yarn add select2")
-      system("yarn add daterangepicker")
-      system("yarn add jgrowl")
-    end
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('templates', __dir__)
 
-    def copy_layout_file
-      copy_file "layouts/_side_navbar.html.slim", "app/views/layouts/_side_navbar.html.slim"
-      copy_file "layouts/_navbar.html.slim", "app/views/layouts/_navbar.html.slim"
-      remove_file "app/views/layouts/application.html.erb"
-      copy_file "layouts/application.html.slim", "app/views/layouts/application.html.slim"
-    end
-
-    def copy_asset_file
-      copy_file "assets/images/cm.png", "app/assets/images/cm.png"
-      directory "assets/stylesheets/", "app/assets/stylesheets/"
-      remove_file "app/assets/stylesheets/application.css"
+      def copy_initializer
+        copy_file 'cm_admin_initializer.rb', 'config/initializers/cm_admin.rb'
+        route 'mount CmAdmin::Engine => "/admin"'
+      end
     end
   end
 end
