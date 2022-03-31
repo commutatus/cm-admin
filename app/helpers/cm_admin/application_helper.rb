71dae82818fc35f124432f4620a94d2221f02ebb
anbublacky <anbublacky@gmail.com>
2022-03-31 17:45:18 +0530

    Update app/helpers/cm_admin/application_helper.rb
    Co-authored-by: Michael Kevin Victor <me@mikevictor.me>

--- app/helpers/cm_admin/application_helper.rb
+++ app/helpers/cm_admin/application_helper.rb
@@ -8,7 +8,7 @@ module CmAdmin
       CmAdmin.webpacker
     end
 
-    # Allow if policy is defined.
+    # Allow if policy is not defined.
     def has_valid_policy(model_name, action_name)
       return true unless policy([:cm_admin, model_name.classify.constantize]).methods.include?(:"#{action_name}?")
       policy([:cm_admin, model_name.classify.constantize]).send(:"#{action_name}?")
