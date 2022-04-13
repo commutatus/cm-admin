655bfa71bdf3a8549d615010d00aa7ce2fadbcae
anbublacky <anbublacky@gmail.com>
2022-04-13 15:52:29 +0530

    Enum class mapper (#156)
    * fix: tag class option added
    
    * fix: Removed info class and fallback to status-tag class
    
    * fix: tag class added for field class

--- lib/cm_admin/models/field.rb
+++ lib/cm_admin/models/field.rb
@@ -3,13 +3,18 @@ module CmAdmin
     class Field
 
       attr_accessor :field_name, :label, :header, :field_type, :format, :precision,
-        :helper_method, :preview, :custom_link, :precision, :prefix, :suffix
+        :helper_method, :preview, :custom_link, :precision, :prefix, :suffix, :tag_class
 
       def initialize(field_name, attributes = {})
         @field_name = field_name
+        set_default_values
         attributes.each do |key, value|
           self.send("#{key.to_s}=", value)
         end
+      end
+
+      def set_default_values
+        self.tag_class = {}
       end
     end
   end
