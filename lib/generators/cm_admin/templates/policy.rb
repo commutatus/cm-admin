f1a9631a1fc19e79b87f37578a4607698ff7689c
anbublacky <anbublacky@gmail.com>
2022-04-21 16:18:25 +0530

    fix: Show page accepting partial
    - Raising error if the model is not present inside the template.

--- lib/generators/cm_admin/templates/policy.rb
+++ lib/generators/cm_admin/templates/policy.rb
@@ -1,5 +1,7 @@
 class CmAdmin::<%= class_name %>Policy < ApplicationPolicy
-  <%- available_action_names = (CmAdmin::Model.find_by({name: class_name}).available_actions.map{|action| action.name}.uniq - ['custom_action', 'new', 'edit']) %>
+  <%- cm_model = CmAdmin::Model.find_by({name: class_name}) %>
+  <%- raise "cm_admin is not defined inside #{class_name} model" unless cm_model.present? %>
+  <%- available_action_names = (cm_model.available_actions.map{|action| action.name}.uniq - ['custom_action', 'new', 'edit']) %>
   <%- available_action_names.each do |action_name| %>
   def <%= action_name %>?
     <%= CmAdmin.authorized_roles.map {|role| "@user.#{role}" }.join(' && ') %>
