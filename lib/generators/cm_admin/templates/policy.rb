b88de9dd35e51a64699b9019807c63771cc2e15b
anbublacky <anbublacky@gmail.com>
2022-04-29 12:13:38 +0530

    fix: Empty policy generator wont be created now

--- lib/generators/cm_admin/templates/policy.rb
+++ lib/generators/cm_admin/templates/policy.rb
@@ -1,6 +1,4 @@
 class CmAdmin::<%= class_name %>Policy < ApplicationPolicy
-  <%- cm_model = CmAdmin::Model.find_by({name: class_name}) %>
-  <%- raise "cm_admin is not defined inside #{class_name} model" unless cm_model.present? %>
   <%- available_action_names = (cm_model.available_actions.map{|action| action.name}.uniq - ['custom_action', 'new', 'edit']) %>
   <%- available_action_names.each do |action_name| %>
   def <%= action_name %>?
