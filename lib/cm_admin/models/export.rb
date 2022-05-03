d91dc87a13d0bf9e340772699979f45be3b29cf1
anbublacky <anbublacky@gmail.com>
2022-05-03 11:34:31 +0530

    Export issues (#165)
    * feat(export): Export with Filtered data - JS changes only
    
    - Controller level changes to be added
    
    * fix: Exporting required data and columns
    
    - Moved a method out of private
    - Got data after policy scope

--- lib/cm_admin/models/export.rb
+++ lib/cm_admin/models/export.rb
@@ -2,24 +2,29 @@ module CmAdmin
   module Models
     class Export
       class << self
-        def generate_excel(klass_name, columns = [], helpers)
+        def generate_excel(klass_name, params, helpers)
           klass = klass_name.constantize
+          columns = params[:columns] || []
+          # filter_params = params[:filters]
           model = CmAdmin::Model.find_by({name: klass_name})
-          records = get_records(klass, model, columns, helpers)
+          # records = get_records(klass, model, columns, helpers)
+          records = "CmAdmin::#{klass_name}Policy::Scope".constantize.new(Current.user, klass).resolve
+          filtered_data = CmAdmin::Models::Filter.filtered_data(model.filter_params(params), records, model.filters)
+          formatted_data = format_records(filtered_data, model, columns, helpers)
           file_path = "#{Rails.root}/tmp/#{klass}_data_#{DateTime.now.strftime("%Y-%m-%d_%H-%M-%S")}.xlsx"
-          create_workbook(records, columns, file_path)
+          create_workbook(formatted_data, columns, file_path)
           return file_path
         end
 
-        def get_records(klass, model, columns, helpers)
-          records = klass
+
+        def format_records(records, model, columns, helpers)
           custom_fields = model.available_fields[:index].map{|field| field if field.field_type == :custom}.compact
           normal_fields = model.available_fields[:index].map{|field| field unless field.field_type == :custom}.compact
           deserialized_columns = CmAdmin::Utils.deserialize_csv_columns(columns, :as_json_params)
           # This includes isn't recursve, a full solution should be recursive
           records_arr = []
           records.includes(deserialized_columns[:include].keys).find_each do |record|
-            record_hash = record.as_json({only: normal_fields.map(&:field_name)})
+            record_hash = record.as_json({only: columns.map(&:to_sym)})
             custom_fields.each do |field|
               record_hash[field.field_name.to_sym] = helpers.send(field.helper_method, record, field.field_name)
             end
