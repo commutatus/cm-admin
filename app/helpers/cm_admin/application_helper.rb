4c0665c8b5873d13b14b0dd1ff9172be81a13233
DeboDevelop <debajyotidofficial@gmail.com>
2022-06-02 11:41:06 +0530

    Refactored using cutom helper

--- app/helpers/cm_admin/application_helper.rb
+++ app/helpers/cm_admin/application_helper.rb
@@ -13,5 +13,15 @@ module CmAdmin
       return true unless policy([:cm_admin, model_name.classify.constantize]).methods.include?(:"#{action_name}?")
       policy([:cm_admin, model_name.classify.constantize]).send(:"#{action_name}?")
     end
+
+    def find_correct_action_name(action_name)
+      if action_name.to_sym  == :update
+        return :edit
+      elsif action_name.to_sym == :create
+        return :new
+      else
+        return action_name.to_sym
+      end
+    end
   end
 end
