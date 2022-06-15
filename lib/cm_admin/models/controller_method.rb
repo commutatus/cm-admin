428b79a0d2fd8189099ae20c317873df72a135b5
anbublacky <anbublacky@gmail.com>
2022-06-15 12:39:15 +0530

    fix: Creation of records for associated pages
    - Nested page creation possible
    - Redirection to the same page after submission
    - Supports has_many, has_many through, polymorphic

--- lib/cm_admin/models/controller_method.rb
+++ lib/cm_admin/models/controller_method.rb
@@ -58,6 +58,7 @@ module CmAdmin
       def resource_params(params)
         permittable_fields = @permitted_fields || @ar_model.columns.map(&:name).reject { |i| CmAdmin::REJECTABLE_FIELDS.include?(i) }.map(&:to_sym)
         permittable_fields += @ar_model.name.constantize.reflect_on_all_associations.map {|x|
+          next if x.options[:polymorphic]
           if x.klass.name == "ActiveStorage::Attachment"
             if x.class.name.include?('HasOne')
               x.name
