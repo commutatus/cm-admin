8edd05564ef3e24ceaf5c5990684b49a481e3282
Debajyoti Dutta <44782833+DeboDevelop@users.noreply.github.com>
2022-06-21 18:10:13 +0530

    Cm page builder compatibility (#199)
    * fix: Creation of records for associated pages
    
    - Nested page creation possible
    - Redirection to the same page after submission
    - Supports has_many, has_many through, polymorphic
    
    * Added hack for cm-page-builder
    
    * fix: Select option issue with DSL fix
    
    - Converted collection into helper method
    
    * Add comment to explain the page builder issue
    
    * fix: Condition added for the associated model
    
    - Some pages associated model is not present.
    - Some pages inverse of is not present
    - converting downcase to underscore
    
    Co-authored-by: anbublacky <anbublacky@gmail.com>

--- lib/cm_admin/models/controller_method.rb
+++ lib/cm_admin/models/controller_method.rb
@@ -59,7 +59,14 @@ module CmAdmin
         permittable_fields = @permitted_fields || @ar_model.columns.map(&:name).reject { |i| CmAdmin::REJECTABLE_FIELDS.include?(i) }.map(&:to_sym)
         permittable_fields += @ar_model.name.constantize.reflect_on_all_associations.map {|x|
           next if x.options[:polymorphic]
-          if x.klass.name == "ActiveStorage::Attachment"
+          # The first if statement is added for compatibilty with cm-page-builder.
+          # One of the associated models of cm-page-builder was throwing error.
+          # The associated model is CmPageBuilder::Rails::PageComponent.
+          # When using reflection, the value of klass of the above association is uninitialized.
+          # As a result, it was throwing error in the 2nd elsif statement.
+          if x.name == :page_components
+            x.name
+          elsif x.klass.name == "ActiveStorage::Attachment"
             if x.class.name.include?('HasOne')
               x.name
             elsif x.class.name.include?('HasMany')
