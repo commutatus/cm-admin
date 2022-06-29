6580b43aec4affaa31550f1830a15fc8891b0644
anbublacky <anbublacky@gmail.com>
2022-06-29 22:50:24 +0530

    chore: Added comment

--- lib/generators/cm_admin/add_authentication_generator.rb
+++ lib/generators/cm_admin/add_authentication_generator.rb
@@ -5,8 +5,9 @@ module CmAdmin
     class AddAuthenticationGenerator < Rails::Generators::Base
       source_root File.expand_path('templates', __dir__)
 
+      # This generator is used to add authentication, if no auth system is present.
+      # Adds authentication through devise and sets up the current user.
       def add_authentication
-        # puts "Copy files"
         gem "devise"
         generate "devise:install"
         model_name = ask("What would you like the user model to be called? [user]")
