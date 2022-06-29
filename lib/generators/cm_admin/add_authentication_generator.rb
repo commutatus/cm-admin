170dc40b940f415cfa5c99dd6e18132c2463d77f
anbublacky <anbublacky@gmail.com>
2022-06-29 22:48:23 +0530

    feat(auth-generator): Added new generator to add authentication to new project and quickly setup the project.

---
+++ lib/generators/cm_admin/add_authentication_generator.rb
@@ -0,0 +1,28 @@
+require 'rails/generators'
+
+module CmAdmin
+  module Generators
+    class AddAuthenticationGenerator < Rails::Generators::Base
+      source_root File.expand_path('templates', __dir__)
+
+      def add_authentication
+        # puts "Copy files"
+        gem "devise"
+        generate "devise:install"
+        model_name = ask("What would you like the user model to be called? [user]")
+        generate "devise", model_name
+        rake "db:migrate"
+        copy_file 'application_controller.rb', 'app/controllers/cm_admin/application_controller.rb'
+        copy_file 'authentication.rb', 'app/controllers/concerns/authentication.rb'
+        copy_file 'current.rb', 'app/models/current.rb'
+        inject_into_file 'app/models/user.rb', before: "end\n" do <<-'RUBY'
+  # Remove this once role is setup and mentioned in zcm_admin.rb
+  def super_admin?
+    true
+  end
+        RUBY
+        end
+      end
+    end
+  end
+end
