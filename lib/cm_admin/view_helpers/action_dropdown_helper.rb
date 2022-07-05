d2bb42cb520bd444d05f3ca730a0a7ca0dfca6d9
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-07-05 11:31:56 +0530

    Refactor: Permission check while filtering the custom action

--- lib/cm_admin/view_helpers/action_dropdown_helper.rb
+++ lib/cm_admin/view_helpers/action_dropdown_helper.rb
@@ -5,15 +5,22 @@ module CmAdmin
         case action_type
         when 'custom_actions'
           model_object.available_actions.select {
-            |act| act if act.route_type.eql?('member') && [:button, :modal].include?(act.display_type)
+            |act| act if act.route_type.eql?('member') &&
+                         [:button, :modal].include?(act.display_type) &&
+                         act.name.present? &&
+                         has_valid_policy(model_object.name, act.name)
           }
         when 'custom_actions_modals'
           model_object.available_actions.select {
-            |act| act if act.route_type.eql?('member') && act.display_type.eql?(:modal)
+            |act| act if act.route_type.eql?('member') &&
+                         act.display_type.eql?(:modal) &&
+                         act.name.present? &&
+                         has_valid_policy(model_object.name, act.name)
           }
         else
           model_object.available_actions.select {
-            |act| act if act.action_type.eql?(:default) && act.name.eql?(action_type)
+            |act| act if act.action_type.eql?(:default) &&
+                         act.name.eql?(action_type)
           } if has_valid_policy(model_object.name, action_type)
         end
       end
