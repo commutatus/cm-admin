00710fa133fa65e117acde105582848b9608ecae
anbublacky <anbublacky@gmail.com>
2022-07-05 15:18:18 +0530

    Code improvement - Major change (#207)
    * fix: Added the module name to the file
    
    * fix: Adding the initializer back
    
    * chore: linked wiki on the readme page
    
    * fix: Adding custom assets through generator
    
    - Without custom assets the app is failing
    
    * fix: Created a configuration class
    
    - To avoid confusion between the config in the initializer and the config method written in the CmAdmin module.
    - config method was used to initialize the admin models
    - Initilizer config is not related to this
    - Wrote separate class so that all configuration goes into this class.
    - Changes seems to be working. authorized_roles changes needs to be merged from the other PR.
    
    * fix: Removed code
    
    - all_actions was not used. So removed it.
    
    * fix: Removed unwanted methods
    
    * refactor: Moving controller level method to controller file
    
    - created a resource controller and moving the code inside it.
    
    * refactor: Removed the unwanted controller_method file and require line
    
    - Removed the commented respond_to block as well
    
    * fix: for history page crash
    
    * fix: Moving filter params to model as it is shared by different controller
    
    * fix: destroy action added to the improvement
    
    * fix: Image attachment fix and destroy redirection fix
    
    * Update app/controllers/cm_admin/resource_controller.rb
    
    Co-authored-by: Aditya Tiwari <taditya.tiwari007@gmail.com>
    
    * Update app/controllers/cm_admin/resource_controller.rb
    
    Co-authored-by: Aditya Tiwari <taditya.tiwari007@gmail.com>
    
    Co-authored-by: Aditya Tiwari <taditya.tiwari007@gmail.com>

--- lib/cm_admin/model.rb
+++ lib/cm_admin/model.rb
@@ -10,7 +10,6 @@ require_relative 'models/export'
 require_relative 'models/cm_show_section'
 require_relative 'models/tab'
 require_relative 'models/dsl_method'
-require_relative 'models/controller_method'
 require 'pagy'
 require 'axlsx'
 require 'cocoon'
@@ -21,7 +20,6 @@ module CmAdmin
     include Pagy::Backend
     include Models::Blocks
     include Models::DslMethod
-    include Models::ControllerMethod
     attr_accessor :available_actions, :actions_set, :available_fields, :permitted_fields,
       :current_action, :params, :filters, :available_tabs, :icon_name
     attr_reader :name, :ar_model, :is_visible_on_sidebar
@@ -89,12 +87,13 @@ module CmAdmin
       @icon_name = name
     end
 
+    # Shared between export controller and resource controller
     def filter_params(params)
       # OPTIMIZE: Need to check if we can permit the filter_params in a better way
-      date_columns = @filters.select{|x| x.filter_type.eql?(:date)}.map(&:db_column_name)
-      range_columns = @filters.select{|x| x.filter_type.eql?(:range)}.map(&:db_column_name)
-      single_select_columns = @filters.select{|x| x.filter_type.eql?(:single_select)}.map(&:db_column_name)
-      multi_select_columns = @filters.select{|x| x.filter_type.eql?(:multi_select)}.map{|x| Hash["#{x.db_column_name}", []]}
+      date_columns = self.filters.select{|x| x.filter_type.eql?(:date)}.map(&:db_column_name)
+      range_columns = self.filters.select{|x| x.filter_type.eql?(:range)}.map(&:db_column_name)
+      single_select_columns = self.filters.select{|x| x.filter_type.eql?(:single_select)}.map(&:db_column_name)
+      multi_select_columns = self.filters.select{|x| x.filter_type.eql?(:multi_select)}.map{|x| Hash["#{x.db_column_name}", []]}
 
       params.require(:filters).permit(:search, date: date_columns, range: range_columns, single_select: single_select_columns, multi_select: multi_select_columns) if params[:filters]
     end
@@ -104,7 +103,7 @@ module CmAdmin
     # Controller defined for each model
     # If model is User, controller will be UsersController
     def define_controller
-      klass = Class.new(CmAdmin::ApplicationController) do
+      klass = Class.new(CmAdmin::ResourceController) do
         include Pundit::Authorization
         rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -116,66 +115,8 @@ module CmAdmin
             @model.params = params
             @action = CmAdmin::Models::Action.find_by(@model, name: action_name)
             @model.current_action = @action
-            @ar_object = @model.try(@action.parent || action_name, params)
-            @ar_object, @associated_model, @associated_ar_object = @model.custom_controller_action(action_name, params.permit!) if !@ar_object.present? && params[:id].present?
-            authorize controller_name.classify.constantize, policy_class: "CmAdmin::#{controller_name.classify}Policy".constantize if defined? "CmAdmin::#{controller_name.classify}Policy".constantize
-            aar_model = request.url.split('/')[-2].classify.constantize  if params[:aar_id]
-            @associated_ar_object = aar_model.find(params[:aar_id]) if params[:aar_id]
-            nested_tables = @model.available_fields[:new].except(:fields).keys
-            nested_tables += @model.available_fields[:edit].except(:fields).keys
-            @reflections = @model.ar_model.reflect_on_all_associations
-            nested_tables.each do |table_name|
-              reflection = @reflections.select {|x| x if x.name == table_name}.first
-              if reflection.macro == :has_many
-                @ar_object.send(table_name).build if action_name == "new" || action_name == "edit"
-              else
-                @ar_object.send(('build_' + table_name.to_s).to_sym) if action_name == "new"
-              end
-            end
-            respond_to do |format|
-              if %w(show index new edit).include?(action_name)
-                if request.xhr? && action_name.eql?('index')
-                  format.html { render partial: '/cm_admin/main/table' }
-                else
-                  format.html { render '/cm_admin/main/'+action_name }
-                end
-              elsif %w(create update destroy).include?(action_name)
-                if params["referrer"]
-                  redirect_url = params["referrer"]
-                elsif %w(create update).include?(action_name)
-                  redirect_url = CmAdmin::Engine.mount_path + "/#{@model.name.underscore.pluralize}/#{@ar_object.id}"
-                else
-                  redirect_url = CmAdmin::Engine.mount_path + "/#{@model.name.underscore.pluralize}"
-                end
-                if action_name.eql?('destroy') || @ar_object.save
-                  format.html { redirect_to  redirect_url, notice: "#{action_name.titleize} #{@ar_object.class.name.downcase} is successful" }
-                else
-                  format.html { render '/cm_admin/main/new', notice: "#{action_name.titleize} #{@ar_object.class.name.downcase} is unsuccessful" }
-                end
-              elsif action.action_type == :custom
-                if action.child_records
-                  format.html { render action.layout }
-                elsif action.display_type == :page
-                  data = @action.parent == "index" ? @ar_object.data : @ar_object
-                  format.html { render action.partial }
-                else
-                  ar_object = @action.code_block.call(@ar_object)
-                  if ar_object.errors.empty?
-                    redirect_url = @model.current_action.redirection_url || @action.redirection_url || request.referrer || "/cm_admin/#{@model.ar_model.table_name}/#{@ar_object.id}"
-                    format.html { redirect_to redirect_url, notice: "#{@action.name.titleize} is successful" }
-                  else
-                    error_messages = ar_object.errors.full_messages.map{|error_message| "<li>#{error_message}</li>"}.join
-                    format.html { redirect_to request.referrer, alert: "<b>#{@action.name.titleize} is unsuccessful</b><br /><ul>#{error_messages}</ul>" }
-                  end
-                end
-              elsif action.layout.present?
-                if request.xhr? && action.partial.present?
-                  format.html { render partial: action.partial }
-                else
-                  format.html { render action.layout }
-                end
-              end
-            end
+            send(@action.controller_action_name, params)
+            # @ar_object = @model.try(@action.parent || action_name, params)
           end
         end
         private
