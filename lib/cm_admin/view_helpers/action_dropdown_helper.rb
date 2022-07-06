cffa07c2441b2317abb52d657a1047b4a5acf7bd
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-07-06 21:07:47 +0530

    Refactor: Change the case condition to if/else loop

--- lib/cm_admin/view_helpers/action_dropdown_helper.rb
+++ lib/cm_admin/view_helpers/action_dropdown_helper.rb
@@ -1,27 +1,19 @@
 module CmAdmin
   module ViewHelpers
     module ActionDropdownHelper
-      def available_actions(model_object, action_type)
-        case action_type
-        when 'custom_actions'
-          model_object.available_actions.select {
+      def available_actions(cm_model, action_type)
+        if action_type.eql?('custom_actions')
+          cm_model.available_actions.select {
             |act| act if act.route_type.eql?('member') &&
                          [:button, :modal].include?(act.display_type) &&
                          act.name.present? &&
-                         has_valid_policy(model_object.name, act.name)
-          }
-        when 'custom_actions_modals'
-          model_object.available_actions.select {
-            |act| act if act.route_type.eql?('member') &&
-                         act.display_type.eql?(:modal) &&
-                         act.name.present? &&
-                         has_valid_policy(model_object.name, act.name)
+                         has_valid_policy(cm_model.name, act.name)
           }
         else
-          model_object.available_actions.select {
+          cm_model.available_actions.select {
             |act| act if act.action_type.eql?(:default) &&
                          act.name.eql?(action_type)
-          } if has_valid_policy(model_object.name, action_type)
+          } if has_valid_policy(cm_model.name, action_type)
         end
       end
     end
