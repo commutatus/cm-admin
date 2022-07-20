2eeaeda4d5f65f75831c37bccec62e2363d71b7c
anbublacky <anbublacky@gmail.com>
2022-07-20 15:35:12 +0530

    feat(rails7-support): Filters dropdown working

--- app/assets/javascripts/cm_admin/application.js
+++ app/assets/javascripts/cm_admin/application.js
@@ -4,5 +4,5 @@ console.log("hi, again");
 import './scaffolds.js';
 import './form_validation.js'
 import './quick_search.js'
-// import './filters.js'
+import './filters.js'
 import './exports.js'
--- app/assets/javascripts/cm_admin/scaffolds.js
+++ app/assets/javascripts/cm_admin/scaffolds.js
@@ -2,8 +2,18 @@ import jQuery from 'jquery';
 window.jQuery = jQuery // <- "select2" will check this
 window.$ = jQuery
 
+// This is a hack to fix 'process is not defined'
+// Ref article: https://adambien.blog/roller/abien/entry/uncaught_referenceerror_process_is_not
+// Based on this filter dropdown works.
+window.process = {
+  env: {
+      NODE_ENV: 'development'
+  }
+} 
+
 import 'moment'
 import 'bootstrap'
+import '@popperjs/core'
 import 'flatpickr'
 import 'jgrowl'
 jqueryJgrowl()
