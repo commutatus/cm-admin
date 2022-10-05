fb79f731694d91930e6eb6b20899867829cde595
anbublacky <anbublacky@gmail.com>
2022-10-05 12:18:18 +0530

    fix: Setting the default input type to string

--- lib/cm_admin/models/form_field.rb
+++ lib/cm_admin/models/form_field.rb
@@ -5,17 +5,18 @@ module CmAdmin
       VALID_INPUT_TYPES = [:integer, :decimal, :string, :single_select, :multi_select, :date, :date_time, :text, :single_file_upload, :multi_file_upload, :hidden, :rich_text].freeze
 
       def initialize(field_name, input_type, attributes = {})
-        raise ArgumentError, "Kindly select a valid filter type like #{VALID_INPUT_TYPES.sort.to_sentence(last_word_connector: ', or ')} instead of #{input_type} for column #{field_name}" unless VALID_INPUT_TYPES.include?(input_type.to_sym)
         @field_name = field_name
         set_default_values
         attributes.each do |key, value|
           self.send("#{key.to_s}=", value)
         end
+        raise ArgumentError, "Kindly select a valid filter type like #{VALID_INPUT_TYPES.sort.to_sentence(last_word_connector: ', or ')} instead of #{self.input_type} for column #{field_name}" unless VALID_INPUT_TYPES.include?(self.input_type.to_sym)
       end
 
       def set_default_values
         self.disabled = false
         self.label = self.field_name.to_s.titleize
+        self.input_type = :string
       end
     end
   end
