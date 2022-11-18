7b804a2baa7128d9026b19e83669ba7418f5fae0
DeboDevelop <debajyotidofficial@gmail.com>
2022-11-18 15:17:34 +0530

    Refactored the validation in a concern

--- lib/cm_admin/models/column.rb
+++ lib/cm_admin/models/column.rb
@@ -1,6 +1,10 @@
+require_relative 'utils/associations'
+
 module CmAdmin
   module Models
     class Column
+      include Utils::Associations
+
       attr_accessor :field_name, :field_type, :header, :format, :prefix, :suffix, :exportable, :round, :height, :width,
       :cm_css_class, :link, :url, :custom_method, :helper_method, :managable, :lockable, :drawer_partial, :tag_class,
       :display_if, :association_name, :association_type
@@ -40,28 +44,27 @@ module CmAdmin
         self.tag_class = {}
       end
 
-      def validation_for_association
-        return unless field_type.to_s == "association"
+      # def validation_for_association
+      #   return unless field_type.to_s == "association"
 
-        raise ArgumentError, 'Expected association_name and association_type to be present' if association_name.nil? || association_type.nil?
+      #   raise ArgumentError, 'Expected association_name and association_type to be present' if association_name.nil? || association_type.nil?
 
-        if association_type.to_s == 'polymorphic'
-          raise ArgumentError, "Expected field_name - #{field_name} - to be an array of hash. Eg, [{table_name_1: 'column_name_1'}, {table_name_2: 'column_name_2'}]" unless field_name.is_a?(Array)
+      #   if association_type.to_s == 'polymorphic'
+      #     raise ArgumentError, "Expected field_name - #{field_name} - to be an array of hash. Eg, [{table_name_1: 'column_name_1'}, {table_name_2: 'column_name_2'}]" unless field_name.is_a?(Array)
 
-          field_name.each do |element|
-            raise ArgumentError, "Expected element #{element} to be a hash. Eg, [{table_name_1: 'column_name_1'}, {table_name_2: 'column_name_2'}]" unless element.is_a?(Hash)
-          end
-        elsif ['belongs_to', 'has_one'].include? association_type.to_s
-          raise ArgumentError, "Expected field_name - #{field_name} to be a String or Symbol" unless field_name.is_a?(Symbol) || field_name.is_a?(String)
-        end
-      end
+      #     field_name.each do |element|
+      #       raise ArgumentError, "Expected element #{element} to be a hash. Eg, [{table_name_1: 'column_name_1'}, {table_name_2: 'column_name_2'}]" unless element.is_a?(Hash)
+      #     end
+      #   elsif ['belongs_to', 'has_one'].include? association_type.to_s
+      #     raise ArgumentError, "Expected field_name - #{field_name} to be a String or Symbol" unless field_name.is_a?(Symbol) || field_name.is_a?(String)
+      #   end
+      # end
 
       class << self
         def find_by(model, search_hash)
           model.available_fields.find { |i| i.name == search_hash[:name] }
         end
       end
-
     end
   end
 end
