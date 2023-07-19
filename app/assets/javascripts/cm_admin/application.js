fe77b584cb9f86e27f254963576d019ca045ebd8
anbublacky <anbublacky@gmail.com>
2023-07-19 19:53:27 +0530

    fix: Renaming file names and updating attribute names

--- app/assets/javascripts/cm_admin/application.js
+++ app/assets/javascripts/cm_admin/application.js
@@ -1,8 +1,11 @@
 import './scaffolds.js';
-import './cocoon.js'
 import './shared_scaffolds.js';
+
+import './bulk_actions.js'
+import './cocoon.js'
+import './exports.js'
+import './filters.js'
 import './form_validation.js'
 import './quick_search.js'
-import './filters.js'
-import './exports.js'
-import './bulk_actions.js'
+
+
--- app/assets/javascripts/cm_admin/bulk_actions.js
+++ app/assets/javascripts/cm_admin/bulk_actions.js
@@ -1,5 +1,5 @@
-$(document).on('click', '[data-behaviour="bulk-checkbox"]', function(e) {
-  if ($('[data-behaviour="bulk-checkbox"]').is(':checked')) {
+$(document).on('click', '[data-behaviour="bulk-action-checkbox"]', function(e) {
+  if ($('[data-behaviour="bulk-action-checkbox"]').is(':checked')) {
     $('[data-section="bulk-action"]').removeClass('hidden')
     setBulkActionParams()
   } else {
@@ -7,20 +7,20 @@ $(document).on('click', '[data-behaviour="bulk-checkbox"]', function(e) {
   }
 });
 
-$(document).on('click', '[data-behaviour="select-all-checkbox"]', function(e) {
+$(document).on('click', '[data-behaviour="bulk-action-select-all"]', function(e) {
   if ($(this).is(':checked')) {
-    $('[data-behaviour="bulk-checkbox"]').prop('checked', true)
+    $('[data-behaviour="bulk-action-checkbox"]').prop('checked', true)
     $('[data-section="bulk-action"]').removeClass('hidden')
     setBulkActionParams()
   } else {
-    $('[data-behaviour="bulk-checkbox"]').prop('checked', false)
+    $('[data-behaviour="bulk-action-checkbox"]').prop('checked', false)
     $('[data-section="bulk-action"]').addClass('hidden')
   }
 });
 
 function setBulkActionParams() {
   var selected_ids = []
-  $('[data-behaviour="bulk-checkbox"]:checked').each(function(){ selected_ids.push($(this).data('object-id')) })
+  $('[data-behaviour="bulk-action-checkbox"]:checked').each(function(){ selected_ids.push($(this).data('entity-id')) })
   $('[data-section="bulk-action"] [name="selected_ids"]').each(function( i ) {
     $(this).val(selected_ids)
   })
