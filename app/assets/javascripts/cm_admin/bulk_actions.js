ad9ff65cb67311e6c6887ec4167f4543b670387e
anbublacky <anbublacky@gmail.com>
2023-08-01 10:41:52 +0530

    fix: Entity to ar-object attribute name change

--- app/assets/javascripts/cm_admin/bulk_actions.js
+++ app/assets/javascripts/cm_admin/bulk_actions.js
@@ -20,7 +20,7 @@ $(document).on('click', '[data-behaviour="bulk-action-select-all"]', function(e)
 
 function setBulkActionParams() {
   var selected_ids = []
-  $('[data-behaviour="bulk-action-checkbox"]:checked').each(function(){ selected_ids.push($(this).data('entity-id')) })
+  $('[data-behaviour="bulk-action-checkbox"]:checked').each(function(){ selected_ids.push($(this).data('ar-object-id')) })
   $('[data-section="bulk-action"] [name="selected_ids"]').each(function( i ) {
     $(this).val(selected_ids)
   })
