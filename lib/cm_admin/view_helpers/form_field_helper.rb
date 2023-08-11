91f16d8cd6f075baa539ec60318e6a282099e7db
Aditya Tiwari <taditya.tiwari007@gmail.com>
2023-08-11 15:53:33 +0530

    Chore: Cosmatic changes

--- lib/cm_admin/view_helpers/form_field_helper.rb
+++ lib/cm_admin/view_helpers/form_field_helper.rb
@@ -7,7 +7,7 @@ module CmAdmin
         value = cm_field.helper_method ? send(cm_field.helper_method, form_obj.object, cm_field.field_name) : form_obj.object.send(cm_field.field_name)
         is_required = form_obj.object._validators[cm_field.field_name].map(&:kind).include?(:presence)
         required_class = is_required ? 'required' : ''
-        target_action = @model.available_actions.select{|x| x.name == cm_field.target[:action_name].to_s}.first if cm_field.target.present?
+        target_action = @model.available_actions.select { |x| x.name == cm_field.target[:action_name].to_s }.first if cm_field.target.present?
         send("cm_#{cm_field.input_type}_field", form_obj, cm_field, value, required_class, target_action)
       end
 
@@ -123,7 +123,7 @@ module CmAdmin
       end
 
       def format_check_box_options(value, form_obj, cm_field, required_class, target_action)
-        if value.class == Array
+        if value.instance_of?(Array)
           format_check_box_array(value, form_obj, cm_field, required_class, target_action)
         else
           form_obj.check_box cm_field.field_name,
@@ -131,7 +131,7 @@ module CmAdmin
                                class: "normal-input cm-checkbox #{required_class} #{target_action.present? ? 'linked-field-request' : ''}",
                                disabled: cm_field.disabled,
                                data: {
-                                 field_name: cm_field.field_name, 
+                                 field_name: cm_field.field_name,
                                  target_action: target_action&.name,
                                  target_url: target_action&.name ? cm_admin.send("#{@model.name.underscore}_#{target_action&.name}_path") : ''
                                }
@@ -169,7 +169,6 @@ module CmAdmin
         end
       end
 
-
       def format_radio_button_options(options, form_obj)
         content_tag :div do
           options.each do |val, key|
@@ -177,7 +176,7 @@ module CmAdmin
           end
         end
       end
-  
+
       def format_radio_option(val, key, form_obj)
         content_tag :div, class: 'cm-radio-section' do
           concat format_radio_button(val, form_obj)
