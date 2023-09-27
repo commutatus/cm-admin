cf99aeea5f8775c4945992115a7222bf442b0e40
anbublacky <anbublacky@gmail.com>
2023-09-27 17:26:13 +0530

    feat(nested-field): Deeply nested cocoon form UI, Params and DSL changes

--- lib/cm_admin/view_helpers/form_helper.rb
+++ lib/cm_admin/view_helpers/form_helper.rb
@@ -116,8 +116,8 @@ module CmAdmin
 
       def set_nested_form_fields(form_obj, section)
         content_tag(:div) do
-          section.nested_table_fields.keys.each do |key|
-            concat(render partial: '/cm_admin/main/nested_table_form', locals: { f: form_obj, assoc_name: key, section: section })
+          section.nested_table_fields.each do |nested_table_field|
+            concat(render partial: '/cm_admin/main/nested_table_form', locals: { f: form_obj, nested_table_field: nested_table_field })
           end
         end
       end
