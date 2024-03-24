7bcc9a35ba8ae24d58cf20c489943e192d60b4d9
Aditya Tiwari <taditya.tiwari007@gmail.com>
2024-03-24 16:59:16 +0530

    Support for friendly URL (#442)
    * Feat: Add support for friendly id on show, update, & destroy action
    
    * Feat: Friendly redirect URL support
    
    * Feat: Construct friendly URL in action dropdown
    
    * Feat: Friendly URL for destroy action in modal
    
    * Feat: Friendly URL on index page table to redirect to show page
    
    * Chore: Spacing between icon and text
    
    * Feat: Friendly URL for associated field type
    
    * Feat: Respond to friendly id on custom controller action
    
    * Feat: Nested tabs with friendly URL
    
    * Feat: Fetch associated ar object with friendly id
    
    * Chore: Change version

--- lib/cm_admin/view_helpers/field_display_helper.rb
+++ lib/cm_admin/view_helpers/field_display_helper.rb
@@ -102,9 +102,9 @@ module CmAdmin
           if field.association_type.to_s == 'polymorphic'
             association_name = ar_object.send(field.association_name).class.to_s.underscore
             field_name = find_field_name(field, association_name)
-            link_to ar_object.send(field.association_name).send(field_name), cm_admin.send("#{association_name}_show_path", ar_object.send(field.association_name).id)
+            link_to ar_object.send(field.association_name).send(field_name), cm_admin.send("#{association_name}_show_path", ar_object.send(field.association_name))
           elsif ['belongs_to', 'has_one'].include? field.association_type.to_s
-            link_to ar_object.send(field.association_name).send(field.field_name), cm_admin.send("#{field.association_name}_show_path", ar_object.send(field.association_name).id)
+            link_to ar_object.send(field.association_name).send(field.field_name), cm_admin.send("#{field.association_name}_show_path", ar_object.send(field.association_name))
           end
         end
       end
