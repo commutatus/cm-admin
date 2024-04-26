379c33ccdc9c29196d155a285ddc6c5a203a3be6
anbublacky <anbublacky@gmail.com>
2024-04-26 18:21:42 +0530

    feat: Nested fields can now be added or deleted based on the condition from application

--- lib/cm_admin/models/nested_field.rb
+++ lib/cm_admin/models/nested_field.rb
@@ -4,7 +4,7 @@ module CmAdmin
 
       # NestedField is like a container to hold Field and FormField object
 
-      attr_accessor :field_name, :display_type, :fields, :associated_fields, :parent_field, :header, :label, :submit_text
+      attr_accessor :field_name, :display_type, :fields, :associated_fields, :parent_field, :header, :label, :submit_text, :is_deletable, :is_creatable
 
       def initialize(field_name, attributes={})
         @field_name = field_name
@@ -19,6 +19,8 @@ module CmAdmin
         self.fields = []
         self.associated_fields = []
         self.submit_text = "+ Add #{@field_name.to_s.titleize}"
+        self.is_deletable = lambda { |arg| return true }
+        self.is_creatable = lambda { |arg| return true }
       end
 
     end
