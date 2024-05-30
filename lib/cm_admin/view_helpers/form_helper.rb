afad575579e8cc94e1fa2a1895a1f63af51281a3
ayzahamid-cm <ayza@commutatus.com>
2024-05-30 17:09:02 +0500

    fix: Query params after redirect

--- lib/cm_admin/view_helpers/form_helper.rb
+++ lib/cm_admin/view_helpers/form_helper.rb
@@ -126,16 +126,17 @@ module CmAdmin
       end
 
       def set_form_with_sections(resource, entities, url, method)
-        polymorphic_name, associated_id, associated_class = extract_referrer_query_params
-        form_for(resource, url: url, method: method, html: { class: "cm_#{resource.class.name.downcase}_form" } ) do |form_obj|
+        url_with_query_params = extract_query_params(url)
+
+        form_for(resource, url: url_with_query_params || url, method: method, html: { class: "cm_#{resource.class.name.downcase}_form" } ) do |form_obj|
           if params[:referrer]
             concat form_obj.text_field "referrer", class: "normal-input", hidden: true, value: params[:referrer], name: 'referrer'
           end
-          if params[:polymorphic_name].present? || polymorphic_name.present?
-            concat form_obj.text_field ( params[:polymorphic_name] || polymorphic_name ) + '_type', class: "normal-input", hidden: true, value: ( params[:associated_class] || associated_class ).classify
-            concat form_obj.text_field ( params[:polymorphic_name] || polymorphic_name ) + '_id', class: "normal-input", hidden: true, value: ( params[:associated_id] || associated_id )
-          elsif (params[:associated_class] && params[:associated_id] || associated_id.present? && associated_class.present? )
-            concat form_obj.text_field ( params[:associated_class] || associated_class ) + '_id', class: "normal-input", hidden: true, value: ( params[:associated_id] || associated_id )
+          if params[:polymorphic_name].present?
+            concat form_obj.text_field params[:polymorphic_name] + '_type', class: "normal-input", hidden: true, value: params[:associated_class].classify
+            concat form_obj.text_field params[:polymorphic_name] + '_id', class: "normal-input", hidden: true, value: params[:associated_id]
+          elsif params[:associated_class] && params[:associated_id]
+            concat form_obj.text_field params[:associated_class] + '_id', class: "normal-input", hidden: true, value: params[:associated_id]
           end
 
           concat split_form_into_section(resource, form_obj, entities)
@@ -145,18 +146,16 @@ module CmAdmin
       end
 
 
-      def extract_referrer_query_params
-        uri = URI.parse(request.referrer)
+      def extract_query_params(url)
+        query_params = {}
+        if params[:polymorphic_name].present? || params[:associated_id].present? && params[:associated_class].present?
+          query_params[:polymorphic_name] = params[:polymorphic_name]
+          query_params[:associated_id] = params[:associated_id]
+          query_params[:associated_class] = params[:associated_class]
+          query_params[:referrer] = params[:referrer] if params[:referrer].present?
 
-        return unless uri.query.present?
-
-        query_params = CGI.parse(uri.query)
-
-        polymorphic_name = query_params['polymorphic_name'].first
-        associated_id = query_params['associated_id'].first
-        associated_class = query_params['associated_class'].first
-
-        return polymorphic_name, associated_id, associated_class
+          return url + '?' + query_params.to_query unless query_params.empty?
+        end
       end
     end
   end
