807e602a6cda3a07cbdbe4b5a8154ad1d7c0e94b
barath46 <rajbarath999@gmail.com>
2024-06-09 09:18:28 +0530

    fix: Table action tool spacing

--- app/assets/javascripts/cm_admin/shared_scaffolds.js
+++ app/assets/javascripts/cm_admin/shared_scaffolds.js
@@ -28,12 +28,6 @@ $(document).on(
 
 $(document).on("click", ".row-action-cell", function (e) {
   e.stopPropagation();
-  if ($(this).hasClass("opacity-1")) {
-    $(".row-action-cell").removeClass("opacity-1");
-  } else {
-    $(".row-action-cell").removeClass("opacity-1");
-    $(this).addClass("opacity-1");
-  }
   if ($(this).find(".table-export-popup").hasClass("hidden")) {
     return $(this).find(".table-export-popup").removeClass("hidden");
   } else {
