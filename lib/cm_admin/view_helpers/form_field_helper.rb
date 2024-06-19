d62edd63514f14fb222bb510662e23030ef5316a
ayzahamid-cm <ayza@commutatus.com>
2024-06-20 02:29:04 +0500

    Update for other form fields

--- lib/cm_admin/view_helpers/form_field_helper.rb
+++ lib/cm_admin/view_helpers/form_field_helper.rb
@@ -19,25 +19,31 @@ module CmAdmin
 
       def cm_integer_field(form_obj, cm_field, value, required_class, _target_action, _ajax_url)
         form_obj.text_field cm_field.field_name,
-                            class: "field-control #{required_class}",
-                            disabled: cm_field.disabled.call(form_obj.object),
-                            value: value,
-                            placeholder: cm_field.placeholder,
-                            data: { behaviour: 'integer-only' }
-      end
-
-      def cm_decimal_field(form_obj, cm_field, value, required_class, _target_action, _ajax_url)
-        form_obj.text_field cm_field.field_name,
+                            merge_wrapper_options(
+                            {
                               class: "field-control #{required_class}",
                               disabled: cm_field.disabled.call(form_obj.object),
                               value: value,
                               placeholder: cm_field.placeholder,
-                              data: { behaviour: 'decimal-only' }
+                              data: { behaviour: 'integer-only' }
+                            }, cm_field.html_attr )
+      end
+
+      def cm_decimal_field(form_obj, cm_field, value, required_class, _target_action, _ajax_url)
+        form_obj.text_field cm_field.field_name,
+                          merge_wrapper_options({
+                            class: "field-control #{required_class}",
+                            disabled: cm_field.disabled.call(form_obj.object),
+                            value: value,
+                            placeholder: cm_field.placeholder,
+                            data: { behaviour: 'decimal-only' }
+                          }, cm_field.html_attr )
       end
 
       def cm_string_field(form_obj, cm_field, value, required_class, _target_action, _ajax_url)
         form_obj.text_field cm_field.field_name,
-                          merge_wrapper_options({
+                          merge_wrapper_options(
+                          {
                             class: "field-control #{required_class}",
                             disabled: cm_field.disabled.call(form_obj.object),
                             value: value,
@@ -47,31 +53,39 @@ module CmAdmin
 
       def cm_custom_string_field(form_obj, cm_field, value, required_class, _target_action)
         text_field_tag cm_field.html_attr[:name] || cm_field.field_name,
-                        value, class: "field-control #{required_class}",
-                        disabled: cm_field.disabled.call(form_obj.object),
-                        placeholder: cm_field.placeholder
+                          merge_wrapper_options(
+                          {
+                            value: value,
+                            class: "field-control #{required_class}",
+                            disabled: cm_field.disabled.call(form_obj.object),
+                            placeholder: cm_field.placeholder
+                          }, cm_field.html_attr )
       end
 
       def cm_single_select_field(form_obj, cm_field, value, required_class, target_action, ajax_url)
         class_name = ajax_url.present? ? 'select-2-ajax' : 'select-2'
         form_obj.select cm_field.field_name, options_for_select(select_collection_value(form_obj.object, cm_field), form_obj.object.send(cm_field.field_name)),
-                        { include_blank: cm_field.placeholder },
-                        class: "field-control #{required_class} #{class_name}",
-                        disabled: cm_field.disabled.call(form_obj.object),
-                        data: {
-                          field_name: cm_field.field_name,
-                          field_type: 'linked-field',
-                          target_action: target_action&.name,
-                          target_url: target_action&.name ? cm_admin.send("#{@model.name.underscore}_#{target_action&.name}_path") : '',
-                          ajax_url: ajax_url
-                        }
+                        {include_blank: cm_field.placeholder},
+                        merge_wrapper_options(
+                        {
+                          class: "field-control #{required_class} #{class_name}",
+                          disabled: cm_field.disabled.call(form_obj.object),
+                          data: {
+                            field_name: cm_field.field_name,
+                            field_type: 'linked-field',
+                            target_action: target_action&.name,
+                            target_url: target_action&.name ? cm_admin.send("#{@model.name.underscore}_#{target_action&.name}_path") : '',
+                            ajax_url: ajax_url
+                          }
+                        }, cm_field.html_attr )
       end
 
       def cm_custom_single_select_field(form_obj, cm_field, value, required_class, target_action, _ajax_url)
         select_tag cm_field.html_attr[:name] || cm_field.field_name,
                     options_for_select(select_collection_value(form_obj.object, cm_field)),
+                    {include_blank: cm_field.placeholder}
+                    merge_wrapper_options(
                     {
-                      include_blank: cm_field.placeholder,
                       class: "field-control #{required_class} select-2",
                       disabled: cm_field.disabled.call(form_obj.object),
                       data: {
@@ -80,54 +94,72 @@ module CmAdmin
                         target_action: target_action&.name,
                         target_url: target_action&.name ? cm_admin.send("#{@model.name.underscore}_#{target_action&.name}_path") : ''
                       }
-                    }
+                    }, cm_field.html_attr )
       end
 
       def cm_multi_select_field(form_obj, cm_field, value, required_class, target_action, _ajax_url)
         form_obj.select cm_field.field_name,
                         options_for_select(select_collection_value(form_obj.object, cm_field), form_obj.object.send(cm_field.field_name)),
-                        { include_blank: cm_field.placeholder },
-                        class: "field-control #{required_class} select-2",
-                        disabled: cm_field.disabled.call(form_obj.object), multiple: true
+                        {include_blank: cm_field.placeholder}
+                        merge_wrapper_options(
+                        {
+                          class: "field-control #{required_class} select-2",
+                          disabled: cm_field.disabled.call(form_obj.object), multiple: true
+                        }, cm_field.html_attr )
       end
 
       def cm_date_field(form_obj, cm_field, value, required_class, _target_action, _ajax_url)
         form_obj.text_field cm_field.field_name,
+                          merge_wrapper_options(
+                          {
                             class: "field-control #{required_class}",
                             disabled: cm_field.disabled.call(form_obj.object),
                             value: value&.strftime('%d-%m-%Y'),
                             placeholder: cm_field.placeholder,
                             data: { behaviour: 'date-only' }
+                          }, cm_field.html_attr )
       end
 
       def cm_custom_date_field(form_obj, cm_field, value, required_class, _target_action, _ajax_url)
         text_field_tag cm_field.html_attr[:name] || cm_field.field_name, value&.strftime('%d-%m-%Y'),
-                            class: "field-control #{required_class}",
-                            disabled: cm_field.disabled.call(form_obj.object),
-                            value: value&.strftime('%d-%m-%Y'),
-                            placeholder: cm_field.placeholder,
-                            data: { behaviour: 'date-only' }
+                        merge_wrapper_options(
+                        {
+                          class: "field-control #{required_class}",
+                          disabled: cm_field.disabled.call(form_obj.object),
+                          value: value&.strftime('%d-%m-%Y'),
+                          placeholder: cm_field.placeholder,
+                          data: { behaviour: 'date-only' }
+                        }, cm_field.html_attr )
       end
 
       def cm_date_time_field(form_obj, cm_field, value, required_class, _target_action, _ajax_url)
         form_obj.text_field cm_field.field_name,
+                          merge_wrapper_options(
+                          {
                             class: "field-control #{required_class}",
                             disabled: cm_field.disabled.call(form_obj.object),
                             value: value,
                             placeholder: cm_field.placeholder,
                             data: { behaviour: 'date-time' }
+                          }, cm_field.html_attr )
       end
 
       def cm_text_field(form_obj, cm_field, value, required_class, _target_action, _ajax_url)
         form_obj.text_area cm_field.field_name,
-                           class: "field-control #{required_class}",
-                           placeholder: cm_field.placeholder
+                          merge_wrapper_options(
+                          {
+                            class: "field-control #{required_class}",
+                            placeholder: cm_field.placeholder
+                          }, cm_field.html_attr)
       end
 
       def cm_rich_text_field(form_obj, cm_field, value, required_class, _target_action, _ajax_url)
         form_obj.rich_text_area cm_field.field_name,
-                                class: "field-control #{required_class}",
-                                placeholder: cm_field.placeholder
+                                merge_wrapper_options(
+                                {
+                                  class: "field-control #{required_class}",
+                                  placeholder: cm_field.placeholder
+                                }, cm_field.html_attr)
       end
 
       def cm_single_file_upload_field(form_obj, cm_field, _value, required_class, _target_action, _ajax_url)
@@ -147,7 +179,7 @@ module CmAdmin
       def attachment_list(form_obj, cm_field, _value, required_class, _target_action)
         attached = form_obj.object.send(cm_field.field_name)
         return if defined?(::Paperclip) && attached.instance_of?(::Paperclip::Attachment)
-        
+
         content_tag(:div) do
           if attached.class == ActiveStorage::Attached::Many
             attached.each do |attachment|
@@ -199,15 +231,16 @@ module CmAdmin
           format_check_box_array(value, form_obj, cm_field, required_class, target_action)
         else
           form_obj.check_box cm_field.field_name,
-                             {
-                               class: "cm-checkbox #{required_class} #{target_action.present? ? 'linked-field-request' : ''}",
-                               disabled: cm_field.disabled.call(form_obj.object),
-                               data: {
-                                 field_name: cm_field.field_name,
-                                 target_action: target_action&.name,
-                                 target_url: target_action&.name ? cm_admin.send("#{@model.name.underscore}_#{target_action&.name}_path") : ''
-                               }
-                             }
+                            merge_wrapper_options(
+                            {
+                              class: "cm-checkbox #{required_class} #{target_action.present? ? 'linked-field-request' : ''}",
+                                disabled: cm_field.disabled.call(form_obj.object),
+                                data: {
+                                  field_name: cm_field.field_name,
+                                  target_action: target_action&.name,
+                                  target_url: target_action&.name ? cm_admin.send("#{@model.name.underscore}_#{target_action&.name}_path") : ''
+                              }
+                            }, cm_field.html_attr )
         end
       end
 
@@ -229,6 +262,7 @@ module CmAdmin
       def format_check_box_tag(val, form_obj, cm_field, required_class, target_action)
         content_tag :div, class: 'cm-radio-tag' do
           concat form_obj.check_box cm_field.field_name,
+                                    merge_wrapper_options(
                                     {
                                       class: "cm-checkbox #{required_class} #{target_action.present? ? 'linked-field-request' : ''}",
                                       disabled: cm_field.disabled.call(form_obj.object),
@@ -237,7 +271,8 @@ module CmAdmin
                                         target_action: target_action&.name,
                                         target_url: target_action&.name ? cm_admin.send("#{@model.name.underscore}_#{target_action&.name}_path", ':param_1') : ''
                                       }
-                                    }, val
+                                    }, cm_field.html_attr ),
+                                    val
         end
       end
 
@@ -264,7 +299,7 @@ module CmAdmin
 
       def merge_wrapper_options(options, html_attr)
         if html_attr
-           html_attr.merge(options) do |key, oldval, newval|
+          options.merge(html_attr) do |key, oldval, newval|
             case key.to_s
             when "class"
               oldval + " " + newval
