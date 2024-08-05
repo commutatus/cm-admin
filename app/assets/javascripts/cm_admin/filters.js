223c153d832a35fc9dcf79ba2d1dc17cc4761632
anbublacky <anbublacky@gmail.com>
2024-08-05 16:25:44 +0530

    fix: Multiple multi select issue fix

--- app/assets/javascripts/cm_admin/filters.js
+++ app/assets/javascripts/cm_admin/filters.js
@@ -44,7 +44,7 @@ var getFilteredData = function(filterType, filterValue, filterColumn=null) {
   var searchParams = window.location.search
   var searchParamshash = getParamsAsObject(searchParams)
   var tempsearchParamshash = delete searchParamshash['page']
-  if (tempsearchParamshash.length > 0) {
+  if (Object.keys(searchParamshash).length > 0) {
     // Delete the previous applied value for multi_select filter from the
     // searchParams as altering the array with new and old value will create
     // more complicated logic. The new value is passed and structured in
