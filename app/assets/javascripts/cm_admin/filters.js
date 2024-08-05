7d6ef0d500a6ea29f34f5c3a199c3bca3c91cbd5
anbublacky <anbublacky@gmail.com>
2024-08-05 16:44:30 +0530

    fix: removed unused variable

--- app/assets/javascripts/cm_admin/filters.js
+++ app/assets/javascripts/cm_admin/filters.js
@@ -42,8 +42,7 @@ var getFilteredData = function(filterType, filterValue, filterColumn=null) {
   // Generate the queryString by concatenating the filterParams and
   // searchParams that are already applied, if searchParams are present.
   var searchParams = window.location.search
-  var searchParamshash = getParamsAsObject(searchParams)
-  var tempsearchParamshash = delete searchParamshash['page']
+  var searchParamsHash = getParamsAsObject(searchParams)
   if (Object.keys(searchParamshash).length > 0) {
     // Delete the previous applied value for multi_select filter from the
     // searchParams as altering the array with new and old value will create
